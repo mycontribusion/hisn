e36718493024e96007fef0e219975b5e23d4ccca
Ahmad Musa <mycontribusion01@gmail.com>
2026-06-04 02:48:22 +0100

    install

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['apple-touch-icon.png', 'icon.png'],
       manifest: {
         name: 'Hisnul Muslim - Fortress of the Muslim',
         short_name: 'Hisnul Muslim',
@@ -18,13 +18,15 @@ export default defineConfig({
         icons: [
           {
             src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            sizes: '192x192 any',
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            sizes: '512x512 any',
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       },
