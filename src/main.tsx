a0c34ee84d1e43eb2e144ac8781d775ec03551bc
Ahmad Musa <mycontribusion01@gmail.com>
2026-06-22 07:01:30 +0100

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { Analytics } from "@vercel/analytics/next"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <App />
+      <Analytics/>
     </BrowserRouter>
   </React.StrictMode>
 )
